27f4849249d68e51430b72d8676a9cab974b61f8
Gonzal <ibai_gonzalez@euneic.com>
2026-05-14 18:03:00 +0200

    tengo que arreglar Raton.cs

--- Colisiones/Assets/Scripts/CollisionFunctions.cs
+++ Colisiones/Assets/Scripts/CollisionFunctions.cs
@@ -14,56 +14,47 @@ public class CollisionFunctions : MonoBehaviour
     float sphereTolerance;
     [SerializeField] float sphereDistance;
     [SerializeField] bool sphereB;
-    bool cubeB;
-    float cubeRot;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         sphereTolerance = 0.5f;
         sphereB = false;
-        cubeB = false;
-        cubeRot = rCube.transform.rotation.z;
     }
 
     // Update is called once per frame
     void Update()
     {
         PointToAABB();
-        //PointToCircle();
+        PointToCircle();
         PointToOBB();
     }
 
 
-    void PointToAABB()
+    public bool PointToAABB()
     {
         cubeXmax = cube.transform.position.x + 0.5f;
         cubeYmax = cube.transform.position.y + 0.5f;
         cubeYmin = cube.transform.position.y - 0.5f;
         cubeXmin = cube.transform.position.x - 0.5f;
-        if(Input.GetKey(KeyCode.Mouse0))
-        {
-            if(transform.position.x <= cubeXmax && transform.position.x >= cubeXmin && transform.position.y <= cubeYmax && transform.position.y >= cubeYmin && !sphereB)
-            {
-                cube.transform.position = new Vector3 (transform.position.x, transform.position.y, 0);
-            }
-        }
+        if (transform.position.x <= cubeXmax && transform.position.x >= cubeXmin && transform.position.y <= cubeYmax && transform.position.y >= cubeYmin) { return true; }
+        else { return false; }
     }
-/*
-    void PointToCircle()
+
+    public bool PointToCircle()
     {
         sphereDistance = Mathf.Sqrt((transform.position.x - sphere.transform.position.x) * (transform.position.x - sphere.transform.position.x) + (transform.position.y - sphere.transform.position.y) * (transform.position.y - sphere.transform.position.y));
-    }
-*/
-    void PointToOBB()
+        if (sphereDistance < sphereTolerance) { return true; }
+        else { return false; }
+    }    
+
+    public bool PointToOBB()
     {
         Vector3 delta = transform.position - rCube.transform.position;
         Vector3 up = rCube.transform.up.normalized;
         Vector3 side = rCube.transform.right.normalized;
         float pUp = Vector3.Dot(delta, up);
         float pRight = Vector3.Dot(delta, side);
-        if (Mathf.Abs(pUp) <= rCube.transform.localScale.y * 0.5f && Mathf.Abs(pRight) <= rCube.transform.localScale.x * 0.5f && Input.GetKey(KeyCode.Mouse0))
-        {
-            rCube.transform.position = new Vector3(transform.position.x, transform.position.y, 0);
-        }
+        if (Mathf.Abs(pUp) <= rCube.transform.localScale.y * 0.5f && Mathf.Abs(pRight) <= rCube.transform.localScale.x * 0.5f) { return true; }
+        else { return false; }
     }
 }
--- Colisiones/Assets/Scripts/Raton.cs
+++ Colisiones/Assets/Scripts/Raton.cs
@@ -8,47 +8,36 @@ public class Raton : MonoBehaviour
     [SerializeField] GameObject sphere;
     [SerializeField]Vector3 mousePos;
     [SerializeField] Camera mainCamera;
-    float cubeXmax;
-    float cubeYmax;
-    float cubeYmin;
-    float cubeXmin;
-    float sphereTolerance;
     [SerializeField] float sphereDistance;
     [SerializeField] bool sphereB;
     [SerializeField] GameObject rCube;
     [SerializeField]bool cubeB;
     [SerializeField]bool cube1B;
-    float pUp;
-    float pRight;
     GameObject current;
+    RaycastHit hit;
+    [SerializeField]
+    float normal;
+    CollisionFunctions collisions;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        sphereTolerance = 0.5f;
         current = null;
+        collisions = GetComponent<CollisionFunctions>();
     }
 
     // Update is called once per frame
     void Update()
-    {
-        cubeXmax = cube.transform.position.x + 0.5f;
-        cubeYmax = cube.transform.position.y + 0.5f;
-        cubeYmin = cube.transform.position.y - 0.5f;
-        cubeXmin = cube.transform.position.x - 0.5f;
+    { 
         Ray ray = mainCamera.ScreenPointToRay(Input.mousePosition);
-        if (Physics.Raycast(ray, out RaycastHit hit))
+        if (Physics.Raycast(ray, out hit))
         {
             transform.position = hit.point;
         }
-        Vector3 delta = transform.position - rCube.transform.position;
-        Vector3 up = rCube.transform.up.normalized;
-        Vector3 side = rCube.transform.right.normalized;
-        pUp = Vector3.Dot(delta, up);
-        pRight = Vector3.Dot(delta, side);
         if (!Input.GetKey(KeyCode.Mouse0))
         {
             current = null;
-            SelectDrag();
+            current = SelectDrag();
+
         }
         else if (Input.GetKey(KeyCode.Mouse0))
         {
@@ -56,44 +45,27 @@ public class Raton : MonoBehaviour
         }
     }
 
-    bool Inside(int i)
-    {
-        switch (i)
-        {
-            case 0:
-                if (transform.position.x <= cubeXmax && transform.position.x >= cubeXmin && transform.position.y <= cubeYmax && transform.position.y >= cubeYmin) {return true;}
-                else {return false;}
-
-            case 1:
-                if (Mathf.Abs(pUp) <= rCube.transform.localScale.y * 0.5f && Mathf.Abs(pRight) <= rCube.transform.localScale.x * 0.5f) {return true;}
-                else {return false;}
-
-            case 2:
-                sphereDistance = Mathf.Sqrt((transform.position.x - sphere.transform.position.x) * (transform.position.x - sphere.transform.position.x) + (transform.position.y - sphere.transform.position.y) * (transform.position.y - sphere.transform.position.y));
-                if (sphereDistance < sphereTolerance) {return true;}
-                else {return false;}
-
-             default:
-                return false;
-        }
-    }
-
     GameObject SelectDrag()
     {
-        if (Inside(0) && current == null)
+        if (hit.transform.gameObject.CompareTag("Cube") && current == null)
         {
-            current = cube;
-            return current;
+            Vector3 vector = Vector3.Cross(hit.transform.up, Vector3.up);
+            normal = vector.normalized.y;
+            if (normal == 1 || normal == 0)
+            {
+                if(collisions.PointToAABB())
+                {
+                    return cube;
+                }
+            }
+            else if(collisions.PointToOBB())
+            {
+                return rCube;
+            }
         }
-        else if (Inside(1) && current == null)
+        else if(collisions.PointToCircle())
         {
-            current = rCube;
-            return current;
-        }
-        else if (Inside(2) && current == null)
-        {
-            current = sphere;
-            return current;
+            return sphere;
         }
         return null;
     }
